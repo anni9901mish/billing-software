49ed8940b2966ef61a7f478bdd38b7a9a0a72b01
ANIMESH MISHRA <animesh.mishra8090@gmail.com>
2026-06-19 15:32:07 +0530

    Fix backend CORS issue

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 
-const productRoutes = require("./routes/products");
-
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
+
+const productRoutes = require("./routes/products");
+app.use("/api/products", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("Inventory Backend Running 🚀");
 });
 
-app.use("/api/products", productRoutes);
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
