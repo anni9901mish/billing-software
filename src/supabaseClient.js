75b0b2e0d7b3ccc82c2f29ca9b28b2dd1369a529
ANIMESH MISHRA <animesh.mishra8090@gmail.com>
2026-06-21 00:36:55 +0530

    fix supabase url

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl =
-  "https://muvxafwfxstdvhxavgdl.supabase.com";
+  "https://muvxafwfxstdvhxavgdl.supabase.co";
 
 const supabaseKey =
   "sb_publishable_oKpQG_OmM0HfusYoGF_Y7g_6PRJLEIE";
